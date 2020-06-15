df6b05413b852b0c9fe5fc2907c5794e7d65d3c4
Ttito19 <andrezchavez.2000@gmaail.com>
2020-06-15 09:37:05 -0500

    crud bus

--- src/components/screens/Buses/addBus.js
+++ src/components/screens/Buses/addBus.js
@@ -1,11 +1,58 @@
-import React, { useState, useEffect } from "React";
+import React, { useState, useEffect } from "react";
+import { useFirebaseApp } from "reactfire";
+import Input from "../../subcomponets/Input";
+function AddBus() {
+  const firebase = useFirebaseApp();
+  const [name, setName] = useState("");
+  const [latitude, setLatitude] = useState(0);
+  const [longitude, setLongitude] = useState(0);
+  const ButtonAddBus = (e) => {
+    e.preventDefault();
 
-function addBus() {
+    firebase
+      .firestore()
+      .collection("BusStop")
+      .add({
+        name,
+        coords: new firebase.firestore.GeoPoint(
+          parseFloat(latitude),
+          parseFloat(longitude)
+        ),
+      })
+      .then(() => setName(""), setLatitude(0), setLongitude(0));
+  };
+
   return (
     <div className="container">
-      <form className="form-group"></form>
+      <form className="form-group">
+        <Input
+          value={name}
+          name="Nombre"
+          type="text"
+          onChange={(e) => setName(e.currentTarget.value)}
+        />
+
+        <Input
+          value={latitude}
+          name="Latitud"
+          type="number"
+          onChange={(e) => setLatitude(e.currentTarget.value)}
+        />
+
+        <Input
+          value={longitude}
+          name="Longitud"
+          type="number"
+          onChange={(e) => setLongitude(e.currentTarget.value)}
+        />
+        <div className="btn pb-2">
+          <button className="btn btn-primary" onClick={ButtonAddBus}>
+            Agregar
+          </button>
+        </div>
+      </form>
     </div>
   );
 }
 
-export default addBus;
+export default AddBus;
--- src/components/subcomponets/Input.js
+++ src/components/subcomponets/Input.js
@@ -2,17 +2,19 @@ import React from "react";
 
 const Input = (props) => {
   // ref.current.focus();
-  const { defaultValue, name, type, onChange, refs } = props;
+  const { value, defaultValue, name, type, onChange, refs } = props;
   return (
     <div className="col-8">
       <label>{props.name}</label>
       <input
+        value={value}
         className="form-control"
         defaultValue={defaultValue}
         name={name}
         type={type}
         onChange={onChange}
         ref={refs}
+        required
       />
     </div>
   );
