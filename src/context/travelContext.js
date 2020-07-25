ff6101d55f7a42e6848eb7cf56e6e58fc3dc5740
TheBilo16 <billy_paredes-2000@hotmail.com>
2020-07-25 16:29:21 -0500

    Pequeño avance * Agregado parte de los input del viaje

--- src/context/travelContext.js
+++ src/context/travelContext.js
@@ -1,8 +1,29 @@
-import React, { createContext } from "react";
+import { firestore } from "firebase";
+import React, { createContext, useEffect } from "react";
 
 const TravelContext = createContext();
 const TravelProvider = ({ children }) => {
 
+  const requestTravel = data => {
+    data.docs.forEach(async (v,i) => {
+      const travel = v.data();
+      const { destiny } = travel;
+
+      try {
+        const bus = await firestore().collection("location").doc(destiny).get();
+        if(bus.exists){      
+          console.log(bus.data());
+        }
+      }catch(e){
+        console.log(e);
+      }
+    })
+  }
+
+  useEffect(() => {
+    const travelEvent = firestore().collection("travel").onSnapshot(requestTravel,console.log);
+    return () => travelEvent();
+  },[])
 
   return <TravelContext.Provider>
     { children }
